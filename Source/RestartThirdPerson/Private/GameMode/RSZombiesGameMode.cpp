3bb164ca63817900d62c5cc77b3b035bf6818b4a
Brandon Hillig <brandonhillig@gmail.com>
2026-07-21 07:59:48 -0400

    Create zombies game mode stub and use it as default project game mode

---
+++ Source/RestartThirdPerson/Private/GameMode/RSZombiesGameMode.cpp
@@ -0,0 +1,5 @@
+// Brandon Hillig 2026
+
+
+#include "GameMode/RSZombiesGameMode.h"
+
---
+++ Source/RestartThirdPerson/Public/GameMode/RSZombiesGameMode.h
@@ -0,0 +1,17 @@
+// Brandon Hillig 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RSZombiesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RESTARTTHIRDPERSON_API ARSZombiesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
