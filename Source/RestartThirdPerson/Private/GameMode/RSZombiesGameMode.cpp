718bd8004401e0f6491103c4a0175837db4b20e6
Brandon Hillig <brandonhillig@gmail.com>
2026-07-22 08:05:16 -0400

    Zombie count for round calculation

--- Source/RestartThirdPerson/Private/GameMode/RSZombiesGameMode.cpp
+++ Source/RestartThirdPerson/Private/GameMode/RSZombiesGameMode.cpp
@@ -3,3 +3,63 @@
 
 #include "GameMode/RSZombiesGameMode.h"
 
+#include "RestartThirdPerson/RestartThirdPerson.h"
+
+void ARSZombiesGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const int32 ZombiesToSpawn = GetZombieCountForRound(CurrentRoundNumber, 1);
+	rs::LogInt("ZombiesToSpawn", ZombiesToSpawn, FColor::Orange, 10.f);
+}
+
+int32 ARSZombiesGameMode::GetZombieCountForRound(int32 RoundNumber, int32 PlayerCount)
+{
+	ensureMsgf(RoundNumber > 0, TEXT("Round number must be at least 1"));
+	ensureMsgf(PlayerCount > 0, TEXT("Player count must be at least 1"));
+
+	const bool bIsSolo = PlayerCount == 1;
+
+	const float ScaleFactor = bIsSolo ? 0.5f : 0.5f + (PlayerCount / 2.f);
+
+	// Before Round 10
+	if (RoundNumber < 10)
+	{
+		float PostScaleFactor = 0.f;
+		switch (RoundNumber)
+		{
+		case 1:
+			PostScaleFactor = 0.25f;
+			break;
+		case 2:
+			PostScaleFactor = 0.3f;
+			break;
+		case 3:
+			PostScaleFactor = 0.7f;
+			break;
+		case 4:
+			PostScaleFactor = 0.9f;
+			break;
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			PostScaleFactor = 1.f;
+			break;
+		default:
+			break;
+		}
+
+		return FMath::Floor((24 + (ScaleFactor * 6 * FMath::Max(1, RoundNumber / 5.f)) * PostScaleFactor));
+	}
+
+	// Round 10 And Above
+	return FMath::Floor(24 + (ScaleFactor * 6 * (RoundNumber/5.f) * RoundNumber * 0.15f));
+}
+
+int32 ARSZombiesGameMode::GetConcurrentZombieCap(int32 PlayerCount)
+{
+	ensureMsgf(PlayerCount > 0, TEXT("Player count must be at least 1"));
+	return 24 + 6 * (PlayerCount - 1);
+}
--- Source/RestartThirdPerson/Public/GameMode/RSZombiesGameMode.h
+++ Source/RestartThirdPerson/Public/GameMode/RSZombiesGameMode.h
@@ -6,6 +6,7 @@
 #include "GameFramework/GameModeBase.h"
 #include "RSZombiesGameMode.generated.h"
 
+class AZombieCharacter;
 /**
  * 
  */
@@ -14,4 +15,22 @@ class RESTARTTHIRDPERSON_API ARSZombiesGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+	/** Called when the simulation has started */
+	virtual void BeginPlay() override;
+
+	/** Returns the number of zombies to spawn for a given round number and player count */
+	int32 GetZombieCountForRound(int32 RoundNumber, int32 PlayerCount);
+
+	/** Returns the max number of concurrent zombies active for a given player count */
+	int32 GetConcurrentZombieCap(int32 PlayerCount);
+
+protected:
+	/** Zombie class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = "Zombie")
+	TSubclassOf<AZombieCharacter> ZombieClass;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	int32 CurrentRoundNumber = 1;
 };
