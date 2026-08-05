184cba6f1cfed26c67a8a211d428dc58ae0951ea
Brandon Hillig <brandonhillig@gmail.com>
2026-08-05 17:51:06 -0400

    Rewrite UI creates/binds to be on the player controller to be MP compatible (part 1)

--- Source/RestartThirdPerson/Private/Controllers/RSPlayerController.cpp
+++ Source/RestartThirdPerson/Private/Controllers/RSPlayerController.cpp
@@ -3,3 +3,66 @@
 
 #include "Controllers/RSPlayerController.h"
 
+#include "ALS/Character/ALSCharacter.h"
+#include "GameStates/RSZombiesGameState.h"
+#include "PlayerStates/RSPlayerState.h"
+
+void ARSPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsLocalPlayerController())
+	{
+		CreateUserWidgets();
+
+		// Try to get the game state
+		if (ARSZombiesGameState* ZombiesGameState = Cast<ARSZombiesGameState>(GetWorld()->GetGameState()))
+		{
+			// Must be on the server
+			BindToGameState(ZombiesGameState);
+		}
+		else
+		{
+			// Listen for replication
+			GetWorld()->GameStateSetEvent.AddLambda([this](AGameStateBase* GameState)
+				{
+					if (ARSZombiesGameState* ZombiesGameState = Cast<ARSZombiesGameState>(GameState))
+					{
+						BindToGameState(ZombiesGameState);
+					}
+				});
+		}
+
+		// Try to get the player state
+		if (ARSPlayerState* PS = GetPlayerState<ARSPlayerState>())
+		{
+			// Must be on server
+			BindToPlayerState(PS);
+		}
+	}
+}
+
+void ARSPlayerController::AcknowledgePossession(APawn* OwningPawn)
+{
+	Super::AcknowledgePossession(OwningPawn);
+
+	if (!IsLocalPlayerController())
+	{
+		return;
+	}
+
+	if (AALSCharacter* PlayerCharacter = Cast<AALSCharacter>(OwningPawn))
+	{
+		BindToPlayerCharacter(PlayerCharacter);
+	}
+}
+
+void ARSPlayerController::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	if (ARSPlayerState* PS = GetPlayerState<ARSPlayerState>())
+	{
+		BindToPlayerState(PS);
+	}
+}
--- Source/RestartThirdPerson/Public/Controllers/RSPlayerController.h
+++ Source/RestartThirdPerson/Public/Controllers/RSPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "RSPlayerController.generated.h"
 
+class AALSCharacter;
+class ARSPlayerState;
+class ARSZombiesGameState;
 /**
  * 
  */
@@ -14,4 +17,30 @@ class RESTARTTHIRDPERSON_API ARSPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	/** Called when the game begins */
+	virtual void BeginPlay() override;
+
+	/** Called when we possess a pawn (Server and clients) */
+	virtual void AcknowledgePossession(APawn* ) override;
+
+	/** Called when the player state replicates */
+	virtual void OnRep_PlayerState() override;
+
+protected:
+	/** Creates user widgets (Blueprints) */
+	UFUNCTION(BlueprintImplementableEvent)
+	void CreateUserWidgets();
+
+	/** Binds callbacks to the game state's delegates (Blueprints) */
+	UFUNCTION(BlueprintImplementableEvent)
+	void BindToGameState(ARSZombiesGameState* ZombiesGameState);
+
+	/** Binds callbacks to the player character delegates (Blueprints) */
+	UFUNCTION(BlueprintImplementableEvent)
+	void BindToPlayerCharacter(AALSCharacter* PlayerCharacter);
+
+	/** Binds callbacks to the player state delegates (Blueprints) */
+	UFUNCTION(BlueprintImplementableEvent)
+	void BindToPlayerState(ARSPlayerState* RSPlayerState);
 };
