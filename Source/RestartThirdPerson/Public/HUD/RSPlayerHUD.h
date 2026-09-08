347907a5dd2452fc5a131a0747159577bf1d7a93
Brandon Hillig <brandonhillig@gmail.com>
2026-09-08 08:18:52 -0400

    Rename PlayerHUD to DamageHUD and prep for Player HUD with new class

---
+++ Source/RestartThirdPerson/Public/HUD/RSPlayerHUD.h
@@ -0,0 +1,17 @@
+// Brandon Hillig 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "RSPlayerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RESTARTTHIRDPERSON_API ARSPlayerHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
